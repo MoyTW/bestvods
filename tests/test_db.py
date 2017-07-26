4a9d8c056e59301ec03fc11179c687ecbda956fb
MoyTW <moytravis@gmail.com>
2017-07-25 21:22:02 -0700

    Flesh out tests for insert game fn

--- tests/test_db.py
+++ tests/test_db.py
@@ -23,8 +23,34 @@ class MyTest(flask_testing.TestCase):
         app.db.session.remove()
         app.db.drop_all()
 
-    def test_game_inserts_once(self):
-        db.insert_game(app.db, 'n', 2017, 'test desc')
+    @staticmethod
+    def test_game_inserts_once():
+        db.insert_game(app.db, 'n', 2004, 'The original')
         result = app.db.engine.execute('select * from game').fetchall()
-        assert result[0] == ('n', 2017, 'test desc')
+        assert result[0] == ('n', 2004, 'The original')
         assert len(result) == 1
+
+    @staticmethod
+    def test_game_inserts_only_once():
+        db.insert_game(app.db, 'n', 2004, 'The original')
+        db.insert_game(app.db, 'n', 2004, 'The original')
+        result = app.db.engine.execute('select * from game').fetchall()
+        assert len(result) == 1
+
+    @staticmethod
+    def test_game_inserts_multiple():
+        db.insert_game(app.db, 'n', 2004, 'The original')
+        db.insert_game(app.db, 'n+', 2008, 'The sequel')
+        db.insert_game(app.db, 'n++', 2015, 'Not a programming language surprisingly')
+        result = app.db.engine.execute('select * from game').fetchall()
+        assert result[2] == ('n++', 2015, 'Not a programming language surprisingly')
+        assert len(result) == 3
+
+    @staticmethod
+    def test_differentiates_by_year():
+        db.insert_game(app.db, 'Doom', 1993, 'Old')
+        db.insert_game(app.db, 'Doom', 2016, 'New')
+        result = app.db.engine.execute('select * from game').fetchall()
+        assert result[0] == ('Doom', 1993, 'Old')
+        assert result[1] == ('Doom', 2016, 'New')
+        assert len(result) == 2
