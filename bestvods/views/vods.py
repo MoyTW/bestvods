182553772409752b044480193ee0b4c3cf7874c5
MoyTW <moytravis@gmail.com>
2017-07-30 18:00:40 -0700

    Move Forms into own ns

--- bestvods/views/vods.py
+++ bestvods/views/vods.py
@@ -1,9 +1,6 @@
+import bestvods.forms as forms
 import bestvods.queries as queries
-import bestvods.validators
-import datetime
 import flask
-import wtforms
-import wtforms.validators as validators
 
 from flask_security import login_required
 from bestvods.database import db
@@ -38,119 +35,10 @@ def root():
     return flask.render_template('_list.html', list_header='VoDs', items=strings)
 
 
-class DateForm(wtforms.Form):
-    year = wtforms.IntegerField('Year', [validators.DataRequired(),
-                                         validators.number_range(min=1962, max=datetime.date.today().year)])
-    month = wtforms.IntegerField('Month', [validators.DataRequired(), validators.number_range(min=1, max=12)])
-    day = wtforms.IntegerField('Day', [validators.DataRequired(), validators.number_range(min=1, max=31)])
-
-    def validate(self):
-        if not super().validate():
-            return False
-
-        try:
-            datetime.date(year=self.year.data, month=self.month.data, day=self.day.data)
-            return True
-        except ValueError:
-            self.day.errors.append('Day ' + str(self.day.data) + ' is not a valid day for ' + str(self.month.data) +
-                                   '!')
-            return False
-
-    @property
-    def date(self) -> datetime.date:
-        return datetime.date(year=self.year.data, month=self.month.data, day=self.day.data)
-
-
-class HHMMSSForm(wtforms.Form):
-    hours = wtforms.IntegerField('Hours', [validators.number_range(min=0, max=24 * 7)])
-    minutes = wtforms.IntegerField('Minutes', [validators.number_range(min=0, max=60)])
-    seconds = wtforms.IntegerField('Seconds', [validators.number_range(min=0, max=60)])
-
-    def validate(self):
-        if not super().validate():
-            return False
-
-        if self.total_seconds <= 0:
-            self.seconds.errors.append('Runs at least take a second!')
-            return False
-        else:
-            return True
-
-    @property
-    def total_seconds(self):
-        return self.hours.data * 60 * 60 + self.minutes.data * 60 + self.seconds.data
-
-
-class RunnersForm(wtforms.Form):
-    runner_exists = bestvods.validators.SatisfiesQuery(db, queries.participant_exists, "No such runner!")
-    runners = wtforms.FieldList(wtforms.StringField('Runner', [validators.DataRequired(), runner_exists]),
-                                min_entries=1)
-    add_runner = wtforms.SubmitField()
-    remove_runner = wtforms.SubmitField()
-
-    def validate(self):
-        if not super().validate():
-            return False
-
-        if len(self.runners.data) > len(frozenset(self.runners.data)):
-            self.runners.errors.append('Runners must be unique!')
-            return False
-        else:
-            return True
-
-
-class CommentatorsForm(wtforms.Form):
-    commentator_exists = bestvods.validators.SatisfiesQuery(db, queries.participant_exists, "No such commentator!")
-    commentators = wtforms.FieldList(wtforms.StringField('Commentator',
-                                                         [validators.DataRequired(), commentator_exists]))
-    add_commentator = wtforms.SubmitField()
-    remove_commentator = wtforms.SubmitField()
-
-    def validate(self):
-        if not super().validate():
-            return False
-
-        if len(self.commentators.data) > len(frozenset(self.commentators.data)):
-            self.commentators.errors.append('Commentators must be unique!')
-            return False
-        else:
-            return True
-
-
-class AddVoDForm(wtforms.Form):
-    # This is kind of silly-looking, I admit. Just, like, formatting-wise.
-    game = wtforms.StringField('Game',
-                               [validators.DataRequired(),
-                                validators.Length(max=256+7),
-                                bestvods.validators.SatisfiesQuery(db,
-                                                                   queries.game_exists,
-                                                                   "I don't know this game!")],
-                               id='game_autocomplete')
-    platform = wtforms.StringField('Platform',
-                                   [validators.DataRequired(),
-                                    validators.Length(max=256),
-                                    bestvods.validators.SatisfiesQuery(db,
-                                                                       queries.platform_exists,
-                                                                       "I don't know this platform!")],
-                                   id='platform_autocomplete')
-    category = wtforms.StringField('Category',
-                                   [validators.DataRequired(),
-                                    validators.Length(max=256),
-                                    bestvods.validators.SatisfiesQuery(db,
-                                                                       queries.category_exists,
-                                                                       "I don't know this category!")],
-                                   id='category_autocomplete')
-    time = wtforms.FormField(HHMMSSForm)
-    date_completed = wtforms.FormField(DateForm)
-    runners = wtforms.FormField(RunnersForm)
-    commentators = wtforms.FormField(CommentatorsForm)
-    add_vod = wtforms.SubmitField()
-
-
 @blueprint.route('/add', methods=['GET', 'POST'])
 @login_required
 def add():
-    form = AddVoDForm(flask.request.form)
+    form = forms.AddVoDForm(flask.request.form)
 
     # TODO: AJAX or JS frontend
     if flask.request.method == 'POST':
