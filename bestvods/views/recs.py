328a6dbde80a5d1f8b4da8e5c712dec30f843a56
MoyTW <moytravis@gmail.com>
2017-08-09 20:35:45 -0700

    Remove username from add URL
    It's not necessary at all!

--- bestvods/views/recs.py
+++ bestvods/views/recs.py
@@ -4,7 +4,7 @@ import sqlalchemy.exc
 
 from bestvods.database import db
 from bestvods.models import UserRec, Vod
-from flask_security import login_required
+from flask_security import login_required, current_user
 
 
 blueprint = flask.Blueprint('recs', __name__, template_folder='templates')
@@ -22,8 +22,8 @@ def root():
     return flask.render_template('_list.html', list_header='Suggested', items=strings)
 
 
-@blueprint.route('/<string:username>/', methods=['GET'])
-def username_root(username):
+@blueprint.route('/list/<string:username>/', methods=['GET'])
+def list_username(username):
     user_recs = UserRec.query.filter(UserRec.user.has(username=username)).limit(50)
     strings = [[rec.user.username,
                 rec.vod.game.name,
@@ -34,9 +34,9 @@ def username_root(username):
     return flask.render_template('_list.html', list_header='Suggested', items=strings)
 
 
-@blueprint.route('/<string:username>/add', methods=['GET', 'POST'])
+@blueprint.route('/add/', methods=['GET', 'POST'])
 @login_required
-def username_add(username):
+def add():
     form = forms.AddUserRecForm(flask.request.form)
 
     if flask.request.method == 'POST':
@@ -51,23 +51,24 @@ def username_add(username):
             return flask.render_template('rec_add.html', form=form, vod_strs=[str(vod) for vod in rows])
 
         elif form.validate():
+            username = current_user.username
             try:
                 user_rec = UserRec.create_with_related(username, form.vod_id.data, form.description.data,
                                                        form.tags.tags.data)
                 db.session.add(user_rec)
                 db.session.commit()
                 flask.flash('Inserted Rec: ' + str(user_rec.id))
-                return flask.redirect(flask.url_for('recs.username_add', username=username))
+                return flask.redirect(flask.url_for('recs.add'))
             except sqlalchemy.exc.IntegrityError:
                 flask.flash('You already recommended vod ' + str(form.vod_id.data))
-                return flask.redirect(flask.url_for('recs.username_add', username=username))
+                return flask.redirect(flask.url_for('recs.add'))
 
     return flask.render_template('rec_add.html', form=form)
 
 
-@blueprint.route('/<string:username>/add/<int:vod_id>', methods=['GET', 'POST'])
+@blueprint.route('/add/<int:vod_id>', methods=['GET', 'POST'])
 @login_required
-def username_add_vod(username, vod_id):
+def username_add_vod(vod_id):
     vod = Vod.query.filter_by(id=vod_id).first()
     if vod is None:
         flask.abort(404)
@@ -82,13 +83,14 @@ def username_add_vod(username, vod_id):
 
         elif form.validate():
             try:
+                username = current_user.username
                 user_rec = UserRec.create_with_related(username, vod_id, form.description.data, form.tags.tags.data)
                 db.session.add(user_rec)
                 db.session.commit()
                 flask.flash('Inserted Rec: ' + str(user_rec.id))
-                return flask.redirect(flask.url_for('recs.username_add', username=username))
+                return flask.redirect(flask.url_for('recs.add'))
             except sqlalchemy.exc.IntegrityError:
                 flask.flash('You already recommended vod ' + str(vod_id))
-                return flask.redirect(flask.url_for('recs.username_add', username=username))
+                return flask.redirect(flask.url_for('recs.add'))
 
     return flask.render_template('rec_add_vod.html', form=form, vod_str=str(vod))
