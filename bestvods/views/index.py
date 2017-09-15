4f71cf0faec12f0bce8a8c3aa8d1dff0567254a7
MoyTW <moytravis@gmail.com>
2017-09-14 20:48:58 -0700

    Experimenting with Flask-Table on Home

--- bestvods/views/index.py
+++ bestvods/views/index.py
@@ -1,11 +1,33 @@
+import datetime
 import flask
+import flask_table
 
 from bestvods.database import db
 from bestvods.models import Vod, UserRec
+from typing import List
 
 blueprint = flask.Blueprint('index', __name__, template_folder='templates')
 
 INDEX_RESULT_LIMIT = 5
+
+
+class SecondsCol(flask_table.Col):
+    def td_format(self, content: int):
+        delta = datetime.timedelta(seconds=content)
+        return str(delta)
+
+
+class VodCountsTable(flask_table.Table):
+    count = flask_table.Col("Recommenders")
+    game = flask_table.LinkCol("Game",
+                               endpoint='games.name_release_year_handler',
+                               attr='game.name_release_year',
+                               url_kwargs=dict(name_release_year='game.name_release_year'))
+#    platform = flask_table.LinkCol("Platform")
+#    category = CategoryCol("Category")
+#    event = EventCol("Event")
+#    runners = RunnersCol("Runners")
+    run_time_seconds = SecondsCol("Time")
 
 
 @blueprint.route('/', methods=['GET'])
@@ -16,14 +38,12 @@ def root():
         .group_by(UserRec.vod_id)\
         .limit(INDEX_RESULT_LIMIT)\
         .all()
-    popular_info = [{"count": row[1],
-                     "game": row[0].game.name_release_year,
-                     "platform": row[0].platform.name,
-                     "category": row[0].category.name,
-                     "event": row[0].event[0].name if len(row[0].event) > 0 else "",
-                     "runners": [runner.handle for runner in row[0].runners],
-                     "time": row[0].run_time_seconds}
-                    for row in vod_counts]
+    table_rows = []
+    for row in vod_counts:
+        table_row = row[0].__dict__
+        table_row['count'] = row[1]
+        table_rows.append(row[0])
+    table = VodCountsTable(table_rows)
 
     newest_results = UserRec.query.order_by(UserRec.timestamp_created.desc()).limit(INDEX_RESULT_LIMIT)
     newest_info = [{"user": row.user.username,
@@ -33,4 +53,4 @@ def root():
                     "tags": [tag.name for tag in row.tags]}
                    for row in newest_results]
 
-    return flask.render_template('index.html', popular_info=popular_info, newest_info=newest_info)
+    return flask.render_template('index.html', newest_info=newest_info, table=table)
